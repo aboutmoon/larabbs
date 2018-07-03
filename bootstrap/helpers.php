b5d6090eae58f4424d8877d872221922193bbb4a
vagrant <vagrant@homestead>
2018-07-03 08:31:58 +0000

    Finish basic page layout

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,3 +2,13 @@
 /**
  * Laravel 提供了很多辅助函数，有时候我们也需要创建自己的辅助函数。
  **/
+
+
+/**
+ * @return mixed
+ * 此方法会将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制。在后面的章节中会用到。
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSRF Token :主要是为了方便前端获取csrf令牌-->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!--继承此模板的页面，如果没有定制 title 区域的话，就会自动使用第二个参数 LaraBBS 作为标题前缀。 -->
+    <title>@yield('title', 'LaraBBS')</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+
+        @include('layouts.header')
+
+        <div class="container">
+
+            @yield('content')
+
+        </div>
+
+        @include('layouts.footer')
+    </div>
+</body>
+</html>
