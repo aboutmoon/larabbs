4e462e05b91157ae887ee4ab36d431cce4751aa7
vagrant <vagrant@homestead>
2018-07-07 13:35:15 +0000

    Add manage contents permission and visit horizon dashboard permission

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,6 +34,9 @@
         @include('layouts.footer')
     </div>
 
+    @if (app()->isLocal())
+        @include('sudosu::user-selector')
+    @endif
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('scripts')
